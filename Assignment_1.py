fe6eaaf43da25187508bfa1cbfe4be0df13fb885
GabrielHuga <144128937+GabrielHuga@users.noreply.github.com>
2023-09-09 12:19:03 +0700

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -6,3 +6,22 @@ str_val = str_val.replace("u","a")
 str_val = str_val.replace("i","a")
 
 print(str_val)
+
+# Nomor 2
+Class_held = 20
+Students = ["Asnawi", "Haaland", "Hazard", "Maudy Ayunan", "Marselino", "Dembele"]
+Class_attended = [18, 19, 10, 20, 17, 13]
+
+def check_attendance(Class_held, Students, Class_attended):
+
+    for i in range(len(Students)):
+        attendance_percentage = (Class_attended[i] / Class_held) * 100
+
+        if attendance_percentage >= 75:
+            hasil = "lebih dari 75%, dapat mengikuti ujian cunin"
+        else:
+            hasil = "kurang dari 75%, tidak dapat mengikuti ujian cunin"
+
+        print(f"Kehadiran {Students[i]} {hasil}")
+
+check_attendance(Class_held, Students, Class_attended)
