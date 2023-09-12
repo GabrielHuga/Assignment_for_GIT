95e7bd8cbbd2e5fe03d055424799c6fc9587d172
GabrielHuga <144128937+GabrielHuga@users.noreply.github.com>
2023-09-12 10:04:59 +0700

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -4,6 +4,7 @@ str_val = "balonku ada lima"
 str_val = str_val.replace("o","a")
 str_val = str_val.replace("u","a")
 str_val = str_val.replace("i","a")
+str_val = str_val.replace("e","a")
 
 print(str_val)
 
