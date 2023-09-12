92e0eb6bfd0c1e56b6a79ca5c9eb6f5aea96f729
GabrielHuga <144128937+GabrielHuga@users.noreply.github.com>
2023-09-12 10:06:13 +0700

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,11 +1,15 @@
 # Nomor 1
+# Nomor 1
 str_val = "balonku ada lima"
 
 str_val = str_val.replace("o","a")
 str_val = str_val.replace("u","a")
 str_val = str_val.replace("i","a")
+str_val = str_val.replace("e","a")
 
 print(str_val)
+
+print("Fitur baruu")
 
 # Nomor 1b
 word = input("Masukkan kata (lowercase): ")
